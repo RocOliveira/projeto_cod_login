b33dd56df79fbafbf970cdc733ed43e2bd302f25
Rodrigo <rocoliveira@hotmail.com>
2025-04-15 22:00:07 -0300

    Criado funções para excluir e editar os nomes da lista

--- main.js
+++ main.js
@@ -22,10 +22,32 @@ if(!loginEmail || !loginSenha){
     if(nomeUser){
         dadosLista.push(nomeUser);
         console.log(dadosLista);
+        criaLista();
         document.getElementById("nomeUser").value=""; 
     } else {
-        alert ("FAvor informar um nome para cadastro!");
+        alert ("Favor informar um nome para cadastro!");
     }
 
-    //Função para preencher a lista de cadastro
+ }
+ 
+ //Função para preencher a lista de cadastro
+ function criaLista(){
+    let tabela = "<tr><th>Nome</th><th>Ações</th></tr>";
+    for (let i = 0; i <= (dadosLista.length-1); i++){
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-warning'onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button class='btn btn-danger' onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
+        document.getElementById("tabela").innerHTML = tabela;
+    }
+ }
+
+ //Função para excluir o nome da lista
+ function excluir(i){
+    dadosLista.splice((i-1),1);
+    document.getElementById("tabela").deleteRow(i);
+        
+ }
+
+ //Função para edição de nome da lista
+ function editar(i){
+    document.getElementById('nomeUser').value = dadosLista[(i - 1)];
+    dadosLista.splice((i-1), 1);
  }
